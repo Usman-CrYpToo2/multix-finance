4df505752ab8171751b380f2b1a2c3c8e71a31d9
Usman Atique <usman.atiq951357@gmail.com>
2026-03-13 22:55:55 +0500

    add Stablecoin.sol implementation

---
+++ src/token/Stablecoin.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IStablecoin } from "../interfaces/IStablecoin.sol";
+
+contract Stablecoin is  IStablecoin, ERC20, Ownable  {
+    address private _mintAndBurnProtocol;
+    
+
+    modifier onlyValidProtocol() {
+        _onlyValidProtocol();
+        _;
+    }
+
+    constructor(
+        address _owner,
+        address _protocol,
+        string memory country,
+        string memory currency
+    ) ERC20(string.concat("Stable_", country), string.concat("ST_", currency)) Ownable(_owner) {
+        require(_protocol != address(0), "Invalid protocol");
+        _mintAndBurnProtocol = _protocol;
+    }
+
+    /// ******************
+    /// * View functions *
+    /// ******************
+
+    function getProtocols() external view returns (address) {
+        return _mintAndBurnProtocol;
+    }
+
+    function isValidProtocol(address _protocol) external view returns (bool) {
+        return _protocol == _mintAndBurnProtocol;
+    }
+
+    /// *******************
+    /// * Owner functions *
+    /// *******************
+    function setMintAndBurnProtocol(address _protocol) external onlyOwner {
+        require(_protocol != address(0), "Invalid protocol");
+        require(_protocol != _mintAndBurnProtocol, "Protocol already set");
+        _mintAndBurnProtocol = _protocol;
+        emit NewMintAndBurnProtocol(_protocol, msg.sender);
+    }
+
+    /// ***********************
+    /// * Protocols functions *
+    /// ***********************
+
+    /// @dev Protocols can only mint token to themself.
+    function mintToValidProtocol(uint256 _amount) external onlyValidProtocol {
+        _mint(msg.sender, _amount);
+
+        emit MintByProtocol(msg.sender, msg.sender, _amount);
+    }
+
+    /// @dev Protocols can only burn token themself, no slashing others.
+    function burnFromValidProtocol(uint256 _amount) external onlyValidProtocol {
+        _burn(msg.sender, _amount);
+
+        emit BurnByProtocol(msg.sender, msg.sender, _amount);
+    }
+
+    /// *********
+    /// * ERC20 *
+    /// *********
+
+    function decimals() public pure override(ERC20, IStablecoin) returns (uint8) {
+        return 6;
+    }
+
+    /// ***********
+    /// * Private *
+    /// ***********
+
+    function _onlyValidProtocol() private view {
+        require(msg.sender == _mintAndBurnProtocol, "Invalid protocol");
+    }
+}
